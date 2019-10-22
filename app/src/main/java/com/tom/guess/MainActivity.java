f3b59d7394973f0ca69d0e5bee2aed6405789530
black <tiger890705@gmail.com>
2019-10-22 17:05:35 +0800

    guess

--- app/src/main/java/com/tom/guess/MainActivity.java
+++ app/src/main/java/com/tom/guess/MainActivity.java
@@ -3,34 +3,57 @@ package com.tom.guess;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
-
+        private EditText num;
+        private TextView message;
+        private TextView Guess;
+        String TAG =MainActivity.class.getName();
+        int secret =new Random().nextInt(10)+1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        num=findViewById(R.id.number);
+        message =findViewById(R.id.message);
+        Guess =findViewById(R.id.guess);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
     }
-
+            public void turn(View view){
+                Scanner scanner = new Scanner(System.in);
+                int guess= Integer.parseInt(num.getText().toString());
+                Log.d(TAG, "secret:"+secret);
+                if(guess==secret){
+                    message.setText("bingo");
+                }
+                else  if(guess<secret) {
+                    message.setText("too small");
+                }
+                else  if(guess>secret){
+                    message.setText(" too big");}
+            }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -51,5 +74,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void Guess(View view){
+
+
     }
 }
